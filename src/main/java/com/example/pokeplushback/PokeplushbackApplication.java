a8acbdd32ddc0b11121a52609b0097b490816d10
AdrianJavier7 <perezhispalis10@gmail.com>
2025-11-03 18:49:53 +0100

    primer commit

---
+++ src/main/java/com/example/pokeplushback/PokeplushbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokeplushback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeplushbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeplushbackApplication.class, args);
+    }
+
+}
