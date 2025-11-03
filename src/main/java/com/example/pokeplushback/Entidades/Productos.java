167ca7f68b9863c37627e9c12fb75ef493533de2
LucDomi <ldominguezsanchez@safareyes.es>
2025-11-03 20:46:39 +0100

    Entidad productos hecha, revisar más tarde por si se cambia la tabla productos.

---
+++ src/main/java/com/example/pokeplushback/Entidades/Productos.java
@@ -0,0 +1,44 @@
+package com.example.pokeplushback.Entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name= "Productos", schema= "pokeplush", catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Productos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column (name="precio")
+    private float precio;
+
+    @Column (name = "tipo")
+    private String tipo;
+
+    @Column (name= "descripcion")
+    private String descripcion;
+
+    @Lob
+    @Column (name= "foto")
+    private Long foto;
+
+    @Column (name= "stock")
+    private int stock;
+
+
+
+
+
+}
