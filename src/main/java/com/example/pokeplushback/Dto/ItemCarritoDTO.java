84d3fe925bfadaf184a043cf282a0785ca75d078
AdrianJavier7 <perezhispalis10@gmail.com>
2025-11-06 19:50:13 +0100

    Postman funcionando carrito

---
+++ src/main/java/com/example/pokeplushback/Dto/ItemCarritoDTO.java
@@ -0,0 +1,11 @@
+package com.example.pokeplushback.Dto;
+
+import lombok.Data;
+
+@Data
+public class ItemCarritoDTO {
+    private Integer id;
+    private Double precioUnitario;
+    private Integer cantidad;
+    private Integer idCarrito;
+}
--- src/main/java/com/example/pokeplushback/Repositorios/ItemsCarritoRepository.java
+++ src/main/java/com/example/pokeplushback/Repositorios/ItemsCarritoRepository.java
@@ -3,6 +3,8 @@ package com.example.pokeplushback.Repositorios;
 import com.example.pokeplushback.Entidades.ItemsCarrito;
 import com.example.pokeplushback.Entidades.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.List;
 @Repository
 public interface ItemsCarritoRepository extends JpaRepository<ItemsCarrito, Integer> {
 
-    // Obtener todos los productos del carrito por el id del carrito
-    List<Productos> findProductosByCarritoId(Integer carritoId);
+    @Query("SELECT ic.producto FROM ItemsCarrito ic WHERE ic.carrito.id = :carritoId")
+    List<Productos> findProductosByCarritoId(@Param("carritoId") Integer carritoId);
+
 
 }
