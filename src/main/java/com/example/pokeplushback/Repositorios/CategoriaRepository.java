542d57fe0d2f1ac0a1b73c7eadbd307c52d24a2f
josemary22 <josemarimateos2005@gmail.com>
2025-12-09 13:46:37 +0100

    Práctica para examen Luis 2

--- src/main/java/com/example/pokeplushback/Repositorios/CategoriaRepository.java
+++ src/main/java/com/example/pokeplushback/Repositorios/CategoriaRepository.java
@@ -3,8 +3,14 @@ package com.example.pokeplushback.Repositorios;
 
 import com.example.pokeplushback.Entidades.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-
+    // Consulta nativa (ejemplo)
+    @Query(value = "SELECT * FROM categorias WHERE nombre = :nombre", nativeQuery = true)
+    List<Categoria> findByNombreNative(@Param("nombre") String nombre);
 }
 */
--- src/main/java/com/example/pokeplushback/conversores/CategoriaMapper.java
+++ src/main/java/com/example/pokeplushback/conversores/CategoriaMapper.java
@@ -1,18 +1,11 @@
-/*
 package com.example.pokeplushback.conversores;
 
-import com.example.pokeplushback.Dto.CategoriaDTO;
-import com.example.pokeplushback.Dto.ProductosDTO;
-import com.example.pokeplushback.Entidades.Categoria;
-import com.example.pokeplushback.Entidades.Productos;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.util.List;
 
 @Mapper(componentModel = "Spring")
 public interface CategoriaMapper {
 
+    /*
     //De entidad a DTO
     @Mapping(source = "fecha_creacion", target = "fechaCreacion", dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     CategoriaDTO convertirADTO(Categoria entity);
@@ -22,5 +15,5 @@ public interface CategoriaMapper {
     @Mapping(source = "fechaCreacion", target = "fecha_creacion", dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     Categoria convertirAEntity(CategoriaDTO dto);
     List<Categoria> convertirAEntity(List<CategoriaDTO> dto);
+     */
 }
-*/
